e4c60f4cd1ffdc7c0afbe210a96f51eff80c70d7
icimpoes <ichimpoes@gmail.com>
2015-01-25 00:53:53 +0200

    return agenda for today

--- src/main/java/com/icimpoes/journal/controller/AgendaController.java
+++ src/main/java/com/icimpoes/journal/controller/AgendaController.java
@@ -1,6 +1,9 @@
 package com.icimpoes.journal.controller;
 
 import java.time.DayOfWeek;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.icimpoes.journal.model.Agenda;
 import com.icimpoes.journal.service.AgendaService;
@@ -18,6 +22,7 @@ import com.icimpoes.journal.util.Messages;
 
 @Controller("agendaController")
 @RequestMapping("/agenda")
+@SessionAttributes({"selectedAgenda"})
 public class AgendaController {
 	
 	@Autowired
@@ -35,9 +40,9 @@ public class AgendaController {
 		return "redirect:view.html";
 	}
 	
-	@RequestMapping(value = "/view", method = RequestMethod.GET)
-	public String viewAgenda() {
-		return "agenda/viewAgendas";
+	@RequestMapping(value = "/viewAll", method = RequestMethod.GET)
+	public String viewAllAgendas() {
+		return "agenda/viewAllAgendas";
 	}
 	
 	@RequestMapping(value = "/getAllAgendas", method = RequestMethod.GET)
@@ -56,7 +61,22 @@ public class AgendaController {
 		return Messages.SUCCESS;
 	}
 	
-	
-	
-	
+	@RequestMapping(value = "/view")
+	public String viewAgenda(Model model){
+		if (!model.containsAttribute("selectedAgenda")) {
+			Agenda agenda = agendaService.findByDayOfWeek(getTodaysWeekDay());
+			model.addAttribute("selectedAgenda", agenda);
+		}
+		return "agenda/viewAgenda";
+	}
+
+	//Return today's Day Of Week
+	private DayOfWeek getTodaysWeekDay() {
+		Calendar calendar = GregorianCalendar.getInstance();
+		calendar.setTime(new Date());
+		int dayNum = calendar.get(Calendar.DAY_OF_WEEK);
+		dayNum = dayNum > 1 ? dayNum - 1 : 7;
+		DayOfWeek day = DayOfWeek.of(dayNum);
+		return day;
+	}
 }
